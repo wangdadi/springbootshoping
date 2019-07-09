a6c33afde434d602849b9ee3538865e4472d6630
17691176276 <1523149063@>
2019-07-09 19:32:28 +0800

    测试上传git

---
+++ src/main/java/com/wd/springboot/springbootshoping/config/TestToGit.java
@@ -0,0 +1,12 @@
+package com.wd.springboot.springbootshoping.config;
+
+/**
+ * ClassName:TestToGit
+ * Package:com.wd.springboot.springbootshoping.config
+ * Description:
+ *
+ * @Date:2019/7/9 0009 19:19
+ * @Author:王迪
+ */
+public class TestToGit {
+}
